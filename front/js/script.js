078a33d49aa96faa12ce59a297f06a05c349e4c0
Priimary <dylan.hazart@gmail.com>
2022-07-28 17:17:30 +0200

    ajout commentaires

--- front/js/script.js
+++ front/js/script.js
@@ -1,9 +1,11 @@
+// récupération données des produits à depuis l'api
 fetch('http://localhost:3000/api/products/')
     .then(function(res) {
         return res.json();
     })
     .then(function(products) {
         let htmlProducts = [];
+        // parcours le tableau de résultat contenant les produits avec une boucle for, ajoutant le code html pour chaque produit dans l'array htmlProducts
         for(let product in products) {
             htmlProducts[product] = `<a href="./product.html?id=${products[product]._id}">
                                         <article>
@@ -13,10 +15,12 @@ fetch('http://localhost:3000/api/products/')
                                         </article>
                                     </a>`
         }
+        // change ensuite le contenu de la section items par le contenu de la variable htmlProducts
         document
             .getElementById('items')
             .innerHTML = htmlProducts.join('');
     })
+    // récupère la possible erreur et l'affiche dans la console, ainsi qu'un message d'erreur pour l'utilisateur
     .catch(function(err) {
         console.log(err);
         document
